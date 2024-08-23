09dc2e477e6a87da9e89407fe4ce13634a31279f
adarsh singh <adsredmi182615@gmail.com>
2024-08-23 23:41:34 +0530

    server work

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv"
 dotenv.config()
 const app = express()
+app.get("/home",(req,res)=>{
+    res.send("testing");
+    
+})
 app.listen(process.env.PORT, () => {
     console.log(`http://localhost:${process.env.PORT}`);
 });
