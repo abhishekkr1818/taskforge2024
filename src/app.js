b7c2f4476e0e1403de33cf93f1c8b4abb3afe31e
adarsh singh <adsredmi182615@gmail.com>
2024-08-23 22:34:14 +0530

    server creation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv from "dotenv"
+dotenv.config()
 const app = express()
-
+app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`);
+});
 export{
     app
 }
