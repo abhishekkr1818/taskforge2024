38c5905b3e5e32abdf3cf0b55c913985a36a2068
adarsh singh <adsredmi182615@gmail.com>
2024-08-23 22:11:43 +0530

    freelancer registration data model

---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema(
+    {
+        firstName:{
+            type:String,
+            required:true
+        },
+        middleName:{
+            type:String
+        },
+        lastName:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:[true,"Password is required"]
+        },
+        profilePicture:{
+            type:String,
+        }
+    },
+    {timestamps:true}
+)
+export const User = mongoose.model("User",userSchema)
