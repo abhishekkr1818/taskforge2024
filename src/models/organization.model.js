b6386103e1bb75bf830b728d53f43bc93eb0d8dd
adarsh singh <adsredmi182615@gmail.com>
2024-08-23 22:18:59 +0530

    Comapny data model

---
+++ src/models/organization.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+const orgSchema = new Schema(
+    {
+        companyName:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    },
+    {timestamps:true}
+)
+export const Org = mongoose.model("Org",orgSchema)
