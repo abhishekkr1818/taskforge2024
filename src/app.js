e15b6ff84c3c05fbf98eec56a598ce0001ebdc5f
adarsh singh <adsredmi182615@gmail.com>
2024-08-25 11:05:59 +0530

    databse connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 dotenv.config()
+connectDB()
 const app = express()
 app.listen(process.env.PORT, () => {
     console.log(`http://localhost:${process.env.PORT}`);
---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+const  connectDB = async ()=>{
+    
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("Connected Successfuly");
+        
+    }
+    catch(error){
+        console.log("DB connection failed",error);
+        process.exit(1)
+        
+    }
+
+}
+export default connectDB
