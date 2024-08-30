173fbc30ee52a6bc7aaebe47fa65d9f5b4c6cad8
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2024-08-30 12:23:21 +0530

    update loginpage

---
+++ src/welcomepage/homescript.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.querySelector('#loginButtonUser');
+    loginButton.addEventListener('click', () => {       
+        window.location.href = '/loginuser';
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.querySelector('#loginButtonOrg');
+    loginButton.addEventListener('click', () => {       
+        window.location.href = '/loginOrg';
+    });
+});
