e61bf70f6285cfcf03555a3cfc9945f18a7bb0d6
adarsh singh <adsredmi182615@gmail.com>
2024-08-31 23:37:43 +0530

    test

--- public/jsfile/post.job.script.js
+++ public/jsfile/post.job.script.js
@@ -15,6 +15,7 @@ document.querySelector('.post-job').addEventListener('submit', async (e) => {
         const data = await response.json();
         if (response.ok) {
             alert("Job posted successfully");
+            window.location.href='/homeorg'
         } else {
             alert("Job not posted");
         }
